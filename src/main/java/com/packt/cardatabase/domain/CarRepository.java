c97a95af415f5af104e5762e0bbc782cb4239e3b
Michael Luo <michael.luo0801@gmail.com>
2020-02-25 22:41:08 +1100

    add resuful servie with spring data reset

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,9 +1,16 @@
 package com.packt.cardatabase.domain;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CarRepository extends CrudRepository <Car, Long> {
     // Fetch cars by brand and sort by year
     List<Car> findByBrandOrderByYearAsc(String brand);
+
+    // Fetch cars by brand
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    // Fetch cars by color
+    List<Car> findByColor(@Param("color") String color);
 }
