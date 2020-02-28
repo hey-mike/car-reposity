564225c4a4423fc2e57fc66caffbdc6f94728fc9
Michael Luo <michael.luo0801@gmail.com>
2020-02-28 22:33:51 +1100

    add auth service

---
+++ src/main/java/com/packt/cardatabase/LoginFilter.java
@@ -0,0 +1,43 @@
+package com.packt.cardatabase;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.packt.cardatabase.domain.AccountCredentials;
+import com.packt.cardatabase.service.AuthenticationService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+    protected LoginFilter(String url, AuthenticationManager authManager) {
+        super(url);
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        AccountCredentials creds = new ObjectMapper().readValue(httpServletRequest.getInputStream(),
+                AccountCredentials.class);
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        creds.getUsername(),
+                        creds.getPassword(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+        AuthenticationService.addToken(response, authResult.getName());
+    }
+}
