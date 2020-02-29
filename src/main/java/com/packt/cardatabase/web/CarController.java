b3ea16cf314926ea6d4c9116e9eb722b8e4013a1
Michael Luo <michael.luo0801@gmail.com>
2020-03-01 09:55:38 +1100

    add unit tests

--- src/main/java/com/packt/cardatabase/web/CarController.java
+++ src/main/java/com/packt/cardatabase/web/CarController.java
@@ -1,16 +1,17 @@
 package com.packt.cardatabase.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.packt.cardatabase.domain.Car;
-import com.packt.cardatabase.domain.CarRepository;
-
 @RestController
 public class CarController {
-    @Autowired
-    private CarRepository carRepository;
+    private final CarRepository carRepository;
+
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
 
     @RequestMapping("/cars")
     public Iterable<Car> getCars() {
